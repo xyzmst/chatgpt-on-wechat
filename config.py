6028eac1b47aa24e42b6fb1916aa6b5cf10dae7b
fanzhihui <fanzhihui@miliantech.com>
2023-04-26 22:44:46 +0800

    #feat 增加日志打印

--- config.py
+++ config.py
@@ -159,12 +159,13 @@ def load_config():
     # 将json字符串反序列化为dict类型
     config = Config(json.loads(config_str))
 
+    logger.info("[INIT] start print config ")
     # override config with environment variables.
     # Some online deployment platforms (e.g. Railway) deploy project from github directly. So you shouldn't put your secrets like api key in a config file, instead use environment variables to override the default config.
     for name, value in os.environ.items():
         name = name.lower()
         if name in available_setting:
-            logger.info("[INIT] override config by environ args: {}={}".format(name, value))
+            logger.info("[INIT] override config by environ xx args: {}={}".format(name, value))
             try:
                 config[name] = eval(value)
             except:
